2e006d72e71975569dc648536ea765e1fd1ce1aa
auroraparaty <auroraeflor@gmail.com>
2025-07-04 00:24:26 -0300

    Update carrossel.js

--- js/carrossel.js
+++ js/carrossel.js
@@ -1,29 +1,21 @@
-function carregarCarrossel(containerId, arquivo) {
-  fetch(arquivo)
+function carregarCarrossel(idContainer, caminhoArquivo) {
+  fetch(caminhoArquivo)
     .then((res) => res.text())
     .then((html) => {
-      document.getElementById(containerId).innerHTML = html;
-      iniciarSwiper(`#${containerId} .mySwiper`);
-    })
-    .catch((erro) => console.error("Erro ao carregar carrossel:", erro));
-}
+      document.getElementById(idContainer).innerHTML = html;
 
-function iniciarSwiper(seletor) {
-  new Swiper(seletor, {
-    loop: true,
-    spaceBetween: 20,
-    centeredSlides: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    pagination: {
-      el: `${seletor} .swiper-pagination`,
-      clickable: true
-    },
-    navigation: {
-      nextEl: `${seletor} .swiper-button-next`,
-      prevEl: `${seletor} .swiper-button-prev`
-    }
-  });
+      new Swiper(`#${idContainer} .swiper`, {
+        loop: false,
+        navigation: {
+          nextEl: `#${idContainer} .swiper-button-next`,
+          prevEl: `#${idContainer} .swiper-button-prev`,
+        },
+        autoplay: false, // DESLIGADO
+        slidesPerView: 1,
+        spaceBetween: 20,
+        centeredSlides: true,
+        grabCursor: true,
+      });
+    })
+    .catch((err) => console.error("Erro ao carregar carrossel:", err));
 }
